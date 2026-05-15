4e0c9b09b76e0b983c68cc9b82d91cd21aa0b8dd
kakeda_misora <kakeda.misora@alhinc.jp>
2026-05-15 10:12:38 +0900

    5/15 コードレビュー1回目(指摘事項の修正済）

--- src/jp/alhinc/calculate_sales/CalculateSales.java
+++ src/jp/alhinc/calculate_sales/CalculateSales.java
@@ -34,6 +34,7 @@ public class CalculateSales {
 	public static void main(String[] args) {
 		// 支店コードと支店名を保持するMap
 		Map<String, String> branchNames = new HashMap<>();
+
 		// 支店コードと売上金額を保持するMap
 		Map<String, Long> branchSales = new HashMap<>();
 
@@ -52,80 +53,69 @@ public class CalculateSales {
 
 		//filesの数だけ繰り返すことで、
 		//指定したパスに存在する全てのファイル(または、ディレクトリ)の数だけ繰り返されます。
-
 		for (int i = 0; i < files.length; i++) {
 			// ファイル名の取得
 			String FileName = files[i].getName();
+
 			//\\2つで次に指定する文字を文字列として扱う
 			if (FileName.matches("^[0-9]{8}\\.rcd")) {
 				// 売上ファイルの条件に当てはまったものだけ、List(ArrayList) に追加します。
 				rcdFiles.add(files[i]);
 			}
 		}
+
 		//rcdFilesに複数の売上ファイルの情報を格納しているので、その数だけ繰り返します。
 		for (int i = 0; i < rcdFiles.size(); i++) {
-			// 売上ファイルを1つずつ読み込む処理（readRcdの呼び出し等）を記述します
+
+			// 売上ファイルを1つずつ読み込む処理
 			// 処理対象のファイルをリストから1つ取り出す
 			File targetFile = rcdFiles.get(i);
-			// 作成した readRcd メソッドをここで呼び出す！
-			if (!readRcd(args[0], targetFile.getName(), branchNames, branchSales)) {
-				return; // 読み込み中にエラーが起きたらプログラムを終了する
+
+			//読み込む対象ファイルオブジェクトを生成
+			File file = new File(args[0], targetFile.getName());
+			// BufferedReaderを使ってファイルを開く（自動クローズ対応）
+			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+				String branchCodeline;
+				String fileSaleline;
+				// コード保持用
+				List<String> codeList = new ArrayList<>();
+				// 売上額保持用
+				List<Long> saleList = new ArrayList<>();
+
+				// br.readLine()で1行目（支店コードと2行目（売上額)を同時に読み込みデータがなくなったらループが自動で終了
+				while ((branchCodeline = br.readLine()) != null && (fileSaleline = br.readLine()) != null) {
+					// 支店コード保持用
+					String branchCode = branchCodeline;
+					//数字（long型)に変換
+					long fileSale = Long.parseLong(fileSaleline);
+
+					//リストへの情報保持
+					codeList.add(branchCode);
+					saleList.add(fileSale);
+
+					//Mapから値を取り出す
+					Long savedSale = branchSales.get(branchCode);
+					// 初期化
+					long currentSale = 0;
+					// savedSale が null ではない時その取れた過去の売上額を currentSale に上書き
+					if (savedSale != null) {
+						currentSale = savedSale;
+					}
+					//合計金額の計算
+					long totalSale = currentSale + fileSale;
+					//Mapへの保存
+					branchSales.put(branchCode, totalSale);
+				}
+			} catch (IOException e) {
+				// 読み込み中にエラーが起きたらエラーメッセージを出してプログラムを終了する
+				System.out.println(UNKNOWN_ERROR);
+				return;
 			}
 		}
 
 		// メインメソッドの最後（すべての集計が終わった後）で書き込みを行います
 		if (!writeFile(args[0], FILE_NAME_BRANCH_OUT, branchNames, branchSales)) {
 			return;
-			}
-	}
-
-	// 売上ファイル読み込み処理
-	private static boolean readRcd(String path, String money, Map<String, String> branchNames,
-			Map<String, Long> branchSales) {
-		BufferedReader br = null;
-		try {
-			File file = new File(path, money);
-			FileReader fr = new FileReader(file);
-			br = new BufferedReader(fr);
-
-			String branchCodeline;
-			String fileSaleline;
-			// 一行ずつ読み込む
-			while ((branchCodeline = br.readLine()) != null && (fileSaleline = br.readLine())!= null) {
-
-				// 分割したデータから「支店コード」と「売上金額」を抜き出す
-				//支店コードを抽出
-				String branchCode = branchCodeline;
-				//売上額を抽出（数字に変換）
-				long fileSale = Long.parseLong(fileSaleline);
-				// 初期値を0にする
-				long currentSale = 0;
-
-				// すでにMapにその支店が存在する場合のみ、現在の売上を取り出す
-				if (branchSales.containsKey(branchCode)) {
-				    currentSale = branchSales.get(branchCode);
-				}
-				// 新しい売上を足し算する
-				long totalSale = currentSale + fileSale;
-				//計算が終わった合計金額（totalSale）をMapに保存する
-				branchSales.put(branchCode, totalSale);
-			}
-			return true;
-
-		} catch (IOException e) {
-			System.out.println(UNKNOWN_ERROR);
-			return false;
-		} finally {
-			// ファイルを開いている場合
-			if (br != null) {
-				try {
-					// ファイルを閉じる
-					br.close();
-				} catch (IOException e) {
-					System.out.println(UNKNOWN_ERROR);
-					return false;
-				}
-			}
 		}
 	}
 
@@ -138,7 +128,6 @@ public class CalculateSales {
 	 * @param 支店コードと売上金額を保持するMap
 	 * @return 読み込み可否
 	 */
-
 	private static boolean readFile(String path, String fileName, Map<String, String> branchNames,
 			Map<String, Long> branchSales) {
 		BufferedReader br = null;
@@ -188,39 +177,38 @@ public class CalculateSales {
 	 * @param 支店コードと売上金額を保持するMap
 	 * @return 書き込み可否
 	 */
-		private static boolean writeFile(String path, String fileName, Map<String, String> branchNames,
-				Map<String, Long> branchSales) {
-			// ※ここに書き込み処理を作成してください。(処理内容3-1)
-			BufferedWriter bw = null;
-			try {
-				File file = new File(path, fileName);
-				FileWriter fw = new FileWriter(file);
-				bw = new BufferedWriter(fw);
+	private static boolean writeFile(String path, String fileName, Map<String, String> branchNames,
+			Map<String, Long> branchSales) {
+		// ※ここに書き込み処理を作成してください。(処理内容3-1)
+		BufferedWriter bw = null;
+		try {
+			File file = new File(path, fileName);
+			FileWriter fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
 
-				for (String key : branchNames.keySet()) {
-					String name = branchNames.get(key);
-					Long sales = branchSales.get(key);
-					String line = key + "," + name + "," + sales;
-					// ファイルへの書き込み処理
-					bw.write(line); // 組み立てた文字列を書き込む
-					bw.newLine();   // 次のループのために改行を入れる
-				}
-					//keyという変数には、Mapから取得したキーが代入されています。
-					//拡張for⽂で繰り返されているので、1つ⽬のキーが取得できたら、
-					//2つ⽬の取得...といったように、次々とkeyという変数に上書きされていきます。
-			}catch(IOException e) {
-				System.out.println(UNKNOWN_ERROR);
-			}finally {
-				try {
-					if (bw != null) {
-						bw.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-					return false;
-				}
+			for (String key : branchNames.keySet()) {
+				String name = branchNames.get(key);
+				Long sales = branchSales.get(key);
+				String line = key + "," + name + "," + sales;
+				// ファイルへの書き込み処理
+				bw.write(line); // 組み立てた文字列を書き込む
+				bw.newLine(); // 次のループのために改行を入れる
 			}
-			return true;
+			//keyという変数には、Mapから取得したキーが代入されています。
+			//拡張for⽂で繰り返されているので、1つ⽬のキーが取得できたら、
+			//2つ⽬の取得...といったように、次々とkeyという変数に上書きされていきます。
+		} catch (IOException e) {
+			System.out.println(UNKNOWN_ERROR);
+		} finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				return false;
+			}
 		}
+		return true;
+	}
 }
-
